51ffa822f47c4849bf53d576a578c6cd458528e3
AlmaPaolaGarciaLanderos <162000855+AlmaPaolaGarciaLanderos@users.noreply.github.com>
2024-05-05 19:35:01 -0600

    Practicas P2 IA

--- Probabilidad-Distribución de Probabilidad.py
+++ Probabilidad-Distribución de Probabilidad.py
@@ -1,3 +1,9 @@
+#Alma Paola Garcia Landeros
+#21110038
+#6E1
+#Inteligencia Artificial
+#Centro de Enseñanza Tecnica Industrial
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
